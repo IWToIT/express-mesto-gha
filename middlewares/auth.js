6093528db420b739841180257374a10295472850
unknown <awesomeman4455@jandex.ru>
2023-01-15 16:04:27 +0100

    изменения для последней пр

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const jsonwebtoken = require('jsonwebtoken');
-const { JWT_SECRET } = require('../constants/constant');
+const { NODE_ENV, JWT_SECRET } = require('../constants/constant');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jsonwebtoken.verify(jwt, JWT_SECRET);
+    payload = jsonwebtoken.verify(jwt, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
